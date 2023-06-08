e36fee4abc891f84f26c3e04ab4140eca93a4b1c
Сергей Гришин <dr_grishin_91@mail.ru>
2023-06-08 18:36:56 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTask_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку: ");
+            string str = Console.ReadLine();
+            string s = null;
+            string resultString = null;
+
+            int counter = 1;
+
+            for (int i = 0; i < str.Length - 1; i++)
+            {
+                s = str[i].ToString();
+                if (str[i] == str[i + 1])
+                {
+                    counter++;
+                }
+                else
+                {
+                    AddToString();
+                }
+
+                if (i == str.Length - 2)
+                {
+                    AddToString();
+                }
+            }
+
+            void AddToString()
+            {
+                resultString += counter.ToString();
+                resultString += s;
+
+                counter = 1;
+                s = null;
+            }
+            Console.WriteLine(resultString);
+        }
+    }
+}
